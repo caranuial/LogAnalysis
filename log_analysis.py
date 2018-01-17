433b26fe724f639a444bbf6b2b9c19459e39764a
caranuial <caranuial@gmail.com>
2018-01-17 20:59:03 +0100

    Update log_analysis.py

--- log_analysis.py
+++ log_analysis.py
@@ -1,48 +1,50 @@
+#! /usr/bin/env python3
+
 import psycopg2
 
-DBNAME = "news"
+
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("<error message>")
 
 
-class DbQuerer():
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select title, count (*) accesses " +
-              "from log join articles on " +
-              "substring(path, 10 , char_length(path)) = articles.slug " +
-              "group by articles.title " +
-              "order by count(*) desc " +
-              "fetch first 3 rows only;")
+def yourFunc():
+    db, cursor = connect()
+
+    query = ("select title, count (*) accesses from log join articles "
+             "on substring(path, 10 , char_length(path)) = articles.slug "
+             "group by articles.title order by count(*) desc fetch first "
+             "3 rows only;")
+    cursor.execute(query)
     print("Top 3 articles of all time:")
     print("")
-    for (title, accesses) in c:
+    for (title, accesses) in cursor:
         print("{} - {} views".format(title, accesses))
     print("")
-    c.execute("select name, count (*) accesses " +
-              "from log join articles on " +
-              "substring(path, 10 , char_length(path)) = articles.slug " +
-              "join authors on articles.author = authors.id " +
-              "group by authors.name " +
-              "order by count(*) desc;")
-    print("Days with more than 1% of requests errors:")
+    query = ("select name, count (*) accesses from log join articles "
+             "on substring(path, 10 , char_length(path)) = articles.slug "
+             "join authors on articles.author = authors.id group by "
+             "authors.name order by count(*) desc;")
+    cursor.execute(query)
+    print("Top 3 articles of all time:")
     print("")
-    for (name, accesses) in c:
+    for (name, accesses) in cursor:
         print("{} - {} views".format(name, accesses))
     print("")
-    c.execute("select a.date as date, " +
-              "round((bad_requests::decimal / requests) * 100,3) " +
-              "as perc_of_bad_req, bad_requests, requests from " +
-              "(select to_char(time, 'dd/mm/yyyy') as date, count(*) " +
-              "bad_requests from log where status <> '200 OK' " +
-              "group by to_char(time, 'dd/mm/yyyy')) as a join " +
-              "(select to_char(time, 'dd/mm/yyyy') as date, count(*) " +
-              "requests from log where path <> '/' group by " +
-              "to_char(time, 'dd/mm/yyyy')) as b on a.date = b.date where " +
-              "(bad_requests::decimal / requests) * 100 >= 1;")
+    query = ("select to_char(date, 'FMMonth FMDD, YYYY') as date, "
+             "err/total*100 as ratio from (select time::date as date, "
+             "count(*) as total, sum((status != '200 OK')::int)::float as "
+             "err from log group by date) as errors where err/total > 0.01;")
+    cursor.execute(query)
     print("Top authors of all time:")
     print("")
-    for (date, perc_of_bad_req, bad_requests, requests) in c:
-        print("{} - {} % errors - bad {} all {}".format(date,
-                                                        perc_of_bad_req,
-                                                        bad_requests,
-                                                        requests))
+    for (date, ratio) in cursor:
+        print("{} - {} % errors".format(date, ratio))
     db.close()
+
+
+yourFunc()
